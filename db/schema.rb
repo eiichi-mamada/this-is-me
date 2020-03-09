a0621aad93a2ad761231d1e76bdd8455dc9ce830
eiichi-mamada <eastrage@gmail.com>
2020-03-10 00:56:08 +0900

    sexualカラムの追加

--- db/schema.rb
+++ db/schema.rb
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2020_03_09_050510) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
+    t.integer "sexual"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
