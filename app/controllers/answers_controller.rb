60857f1df37483f04150f55d559196bc9b31393f
kyo-today <k.kyo0110@gamil.com>
2020-03-12 14:07:50 +0900

    Auto stash before merge of "feature/answers_table" and "origin/master"

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,6 +1,31 @@
 class AnswersController < ApplicationController
 
   def index
+    @answers = Answer.all
+  end
+
+  def new
+    @answer   = Answer.new
+    @questions_past = Question.where(status: 0)
+  end
+
+  def create
+    Answer.create(answer_params)
+    redirect_to root_path
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+
+  private
+
+
+  def answer_params
+    params.require(:answer).permit(:reason, :rank)
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {registrations: 'users/registrations'}
   root to: "home#index"
 
-  resources :restaurants, only: :index 
+  resources :restaurants, only: :index
   namespace :admin do
     resources :questions, except: :show
   end
   resources :answers,   only:  [:index, :new, :create, :edit, :update]
-end
 
+  # get '/answers', to 'home#index'  
+end
