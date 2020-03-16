72fa877b9d0b305d3b008d70ec6b56d5d1598651
eiichi-mamada <eastrage@gmail.com>
2020-03-16 15:33:41 +0900

    余計なルーティング削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {registrations: 'users/registrations'}
   root to: "home#index"
-
-  resources :restaurants, only: :index 
+  
   namespace :admin do
     resources :questions, except: :show
   end
