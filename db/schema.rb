da00bee48407622705e528104da100fce8d4b990
eiichi-mamada <eastrage@gmail.com>
2020-03-16 19:14:23 +0900

    ヘッダー追加

--- db/schema.rb
+++ db/schema.rb
@@ -50,7 +50,7 @@ ActiveRecord::Schema.define(version: 2020_03_12_065824) do
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
     t.integer "sexual"
-    t.date "birthday"
+    t.date "birthday", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
