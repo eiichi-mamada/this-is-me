680046702bc845b8362d5304b25626edef173146
masachan-100 <59827466+masachan-100@users.noreply.github.com>
2020-03-31 18:22:00 +0900

    完了

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -10,13 +10,13 @@ class AnswersController < ApplicationController
   end
 
   def now_new
-    @answer           = Answer.new
-    @questions_now    = Question.where(status: 1)
+    @questions = Question.where(status: 1)
+    @answers = AnswerCollection.new(value: 1)
   end
 
   def future_new
-    @answer           = Answer.new
-    @questions_future = Question.where(status: 2)
+    @questions = Question.where(status: 2)
+    @answers = AnswerCollection.new(value: 2)
   end
 
   def create
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,13 +11,27 @@ class HomeController < ApplicationController
     # @past_communication = past.where(feature: 5)
 
     past_questions = Question.where(status: 0)
+    now_questions = Question.where(status: 1)
+    future_questions = Question.where(status: 2)
     #viewへ渡す配列を定義
 
     @past_total_rank = [0,0,0,0,0,0]
+    @now_total_rank = [0,0,0,0,0,0]
+    @future_total_rank = [0,0,0,0,0,0]
     # answerの中身があり、featureが０から５のそれぞれの合計値を@past_total_rankに入れる
     past_questions.each do |past_question|
       if past_question.answer.present?
         @past_total_rank[past_question.feature] += past_question.answer.rank
+      end
+    end
+    now_questions.each do |now_question|
+      if now_question.answer.present?
+        @now_total_rank[now_question.feature] += now_question.answer.rank
+      end
+    end
+    future_questions.each do |future_question|
+      if future_question.answer.present?
+        @future_total_rank[future_question.feature] += future_question.answer.rank
       end
     end
   end
